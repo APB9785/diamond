f4debc2cb9640aa62982c017244ed6c229dbca6c
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-03-19 21:40:51 -0500

    Add hex config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule Diamond.MixProject do
   use Mix.Project
 
+  @gh_url "https://github.com/APB9785/diamond"
+  @version "0.1.0"
+
   def project do
     [
       app: :diamond,
-      version: "0.1.0",
-      elixir: "~> 1.16",
+      version: @version,
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: "",
+      package: package(),
+
+      # Docs
+      name: "Diamond",
+      docs: docs()
     ]
   end
 
@@ -17,5 +28,32 @@ defmodule Diamond.MixProject do
 
   defp deps do
     [{:ex_doc, "~> 0.20", only: [:docs, :dev]}]
+  end
+
+  defp package do
+    [
+      maintainers: ["Andrew P Berrien"],
+      licenses: ["GPL-3.0"],
+      links: %{
+        "Changelog" => "#{@gh_url}/blob/master/CHANGELOG.md",
+        "GitHub" => @gh_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Diamond",
+      source_ref: "v#{@version}",
+      logo: nil,
+      source_url: @gh_url,
+      extras: extras()
+    ]
+  end
+
+  defp extras do
+    [
+      "CHANGELOG.md"
+    ]
   end
 end
